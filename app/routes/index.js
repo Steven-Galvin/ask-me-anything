c960788cbd53b7ce40c10bcb31208aa42b61d43f
Steven Galvin <steven.john.galvin@gmail.com>
2017-06-02 13:03:06 -0700

    add functionality to creating new question and deleting questions.

--- app/routes/index.js
+++ app/routes/index.js
@@ -4,4 +4,19 @@ export default Ember.Route.extend({
   model() {
     return this.store.findAll('question');
   },
+
+  actions: {
+    saveQuestion(params) {
+      var newQuestion = this.store.createRecord('question', params);
+      newQuestion.save();
+      this.transitionTo('index');
+    },
+
+    destroyQuestion(question) {
+      if (confirm('Are you sure you want to delete this rental?')) {
+        question.destroyRecord();
+        this.transitionTo('index');
+      }
+    }
+  }
 });
